9697e5bfa471de6f33911d890bd082a636decf4f
Mayur Kharat <mayurkharat2006@gmail.com>
2026-03-28 23:45:55 +0530

    Debug: Enabled DEBUG=True on Vercel to diagnose 'Server Error (500)'. This will surface the underlying database or configuration issue.

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&97_z@y6mpjpt54brhddt7@49rx(8h9*%=hxs)k8!_c@@i&dx8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
